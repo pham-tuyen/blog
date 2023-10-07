46506338693688177a3b76233ebf0f6121ea1a8b
Phạm Tuyên <phamthanhtuyen2k8@gmail.com>
2023-10-07 13:57:53 +0700

    update content

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -4,7 +4,7 @@ import "./css/tile.css"
 export default function Tile(props) {
   return (
     <Card style={{ width: '18rem', border: 'none', backgroundColor: 'white' }}>
-      <Card.Img variant="top" className="rounded im" src={require(props.thumbnail)}/>
+      <Card.Img variant="top" className="rounded im" src={props.thumbnail}/>
       <Card.Body>
         <Card.Title className="text-center">{props.title}</Card.Title>
         <Card.Text>
